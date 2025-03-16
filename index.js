11a09c24422f7a563beb1b172e49413cfdc50a20
Silva-NK <kiagonsuzanne@gmail.com>
2025-03-16 21:58:26 +0300

    Adds default image and facilitates it's display.

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-const ramens = [
-    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
-    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
+document.addEventListener("DOMContentLoaded", main);
+
+const ramens = [ {
+    id: 1, 
+    name: "Instant Ramen",
+    description: "A quick affordable meal featuring packaged noodles with a simple broth, chewy noodles and seasoning packet. Can be prepared quickly in the comfort of your own home.",
+    restaurant: "N/A. Commercially sold. Prepared at home.", 
+    image: "images/instant-2.jpg", 
+    rating: 5, 
+    comment: "Easy and delicious." }
  ];
+
+ function main() {
+    displayRamens();
+}
+
+ function displayRamens() {
+    const defaultRamen = ramens[0];
+
+    const defaultImage  = document.getElementById("defaultImage");
+    const ramenName  = document.getElementById("ramenName");
+    const ramenDescription  = document.getElementById("ramenDescription");
+    const ramenRestaurant  = document.getElementById("ramenRestaurant");
+    const ramenRating  = document.getElementById("ramenRating");
+    const ramenComment = document.getElementById("ramenComment");
+
+    defaultImage.src = defaultRamen.image;
+    ramenName.textContent = defaultRamen.name;
+    ramenDescription.textContent = defaultRamen.description;
+    ramenRestaurant.textContent = defaultRamen.restaurant;
+    ramenRating.textContent =`${defaultRamen.rating}/10`;
+    ramenComment.textContent = defaultRamen.comment;
+ }
